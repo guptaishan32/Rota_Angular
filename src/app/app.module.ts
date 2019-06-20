9217aa9a65fa672da4dc20bde9082799f0ed7cdb
Ishan Gupta <guptaishan32@gmail.com>
2019-06-20 13:26:42 +0530

    Index Page YouTube Videos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatGridListModule } from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatExpansionModule} from '@angular/material/expansion';
+
+import { YoutubePlayerModule } from 'ngx-youtube-player';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { YoutubeVideoComponent } from './home/youtube-video/youtube-video.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    YoutubeVideoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, BrowserAnimationsModule,YoutubePlayerModule,
+    MatToolbarModule,MatGridListModule,MatCardModule,MatExpansionModule,
+    FlexLayoutModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/youtube-video/youtube-video.component.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'youtubeVideos',
+  template:`
+    <div class="container-fluid"
+    fxLayout="row"
+    fxLayout.xs="column"
+    fxLayoutGap="10px">
+  <div *ngFor="let video of YouTubeVideos" fxFlex>
+    <div>
+        <p id="vheadings">{{video.title}} <small>- {{video.year}} {{video.subtitle }}</small></p>
+    </div>  
+    <div class="embed-responsive embed-responsive-16by9">
+        <youtube-player
+        width="450" 
+        height="315"
+        videoId="{{video.link}}"
+        (ready)="savePlayer($event)"
+        (change)="onStateChange($event)"
+      ></youtube-player>
+    </div>
+  </div>
+  </div>
+  `,
+  styleUrls: ['./youtube-video.component.scss']
+})
+export class YoutubeVideoComponent implements OnInit {
+
+  title = "Videos";
+  
+  constructor() { }
+
+  YouTubeVideos = [
+    {
+      id:'0',
+      title: 'RACW',
+      subtitle:' ',
+      year: '2k17-18',
+      link: 'cRQCO9ErIyI',
+      featured: true
+    },
+    {
+      id:'1',
+      title: 'Cancer Awareness',
+      subtitle:'7 Projects in a day',
+      year: ' ',
+      link: 'R4qa7xf0fi8?start=6',
+      featured: true
+    },
+    {
+      id:'2',
+      title: 'Friendship Day',
+      subtitle:' ',
+      year: '2k16-17 ',
+      link: 'D66dPIKT8RU?start=2',
+      featured: true
+    }
+  ]
+
+  ngOnInit() {
+  }
+
+  player: YT.Player;
+  private Id:string[] =[ 'cRQCO9ErIyI','R4qa7xf0fi8','D66dPIKT8RU'];
+ 
+  savePlayer(player) {
+    this.player = player;
+    console.log('Video Url', this.player.getVideoUrl);
+  }
+  onStateChange(event) {
+    console.log('player state', event.data);
+  }
+
+  playVideo(){
+    this.player.playVideo();
+  }
+
+  pauseVideo(){
+    this.player.pauseVideo();
+  }
+
+}
